b19a0ac3da3a7145542b3aa18d6c2e67f98c2c80
shashank-kenpath <104070563+shashank-kenpath@users.noreply.github.com>
2025-05-21 12:48:20 +0530

    deployment DB changes

--- services/db.js
+++ services/db.js
@@ -1,12 +1,27 @@
 const { Pool } = require('pg');
+const fs = require('fs');
+const path = require('path');
+
+// const pool = new Pool({
+//   user: process.env.UNIQUE_NAME_PG_USER,
+//   password: process.env.UNIQUE_NAME_PG_PASSWD,
+//   host: process.env.UNIQUE_NAME_PG_HOST,
+//   port: process.env.UNIQUE_NAME_PG_PORT,
+//   database: process.env.UNIQUE_NAME_PG_DB,
+//   ssl: process.env.UNIQUE_NAME_PG_SSL === 'true'
+// });
+
 
 const pool = new Pool({
-  user: process.env.UNIQUE_NAME_PG_USER,
-  password: process.env.UNIQUE_NAME_PG_PASSWD,
-  host: process.env.UNIQUE_NAME_PG_HOST,
-  port: process.env.UNIQUE_NAME_PG_PORT,
-  database: process.env.UNIQUE_NAME_PG_DB,
-  ssl: process.env.UNIQUE_NAME_PG_SSL === 'true'
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: parseInt(process.env.DB_PORT || "5432", 10),
+  ssl: {
+    rejectUnauthorized: true,
+    ca: fs.readFileSync(path.join(__dirname, 'certs', 'rds-global.pem')).toString()
+  }
 });
 
 module.exports = pool;
