d4db635fbb74e79b1940b05edbe847dc463ecbaf
shashank-kenpath <104070563+shashank-kenpath@users.noreply.github.com>
2025-10-14 17:27:18 +0530

    test run

--- services/db.js
+++ services/db.js
@@ -18,7 +18,7 @@ const pool = new Pool({
 // const pool = new Pool({
 //   user: process.env.DB_USER,
 //   host: process.env.DB_HOST,
-//   database: process.env.DB_NAME,
+//   database: process.env.DB_NAME,  
 //   password: process.env.DB_PASSWORD,
 //   port: parseInt(process.env.DB_PORT || "5432", 10),
 //   ssl: {
